1045f518843852e223ab84e4daca737ba9141a3f
alexkoval6030 <alexkoval6030@gmail.com>
2021-07-19 07:35:27 +0300

    c

--- Main/src/by/academy/classwork/lesson4/ProductDemo.java
+++ Main/src/by/academy/classwork/lesson4/ProductDemo.java
@@ -3,8 +3,7 @@ package by.academy.classwork.lesson4;
 public class ProductDemo {
 
 	public static void main(String[] args) {
-		Product phone = new Product("Phone", 150, 21.2);
-		//
+		
 	}
 
 }
--- Main/src/by/academy/classwork/lesson4/Task3.java
+++ Main/src/by/academy/classwork/lesson4/Task3.java
@@ -5,15 +5,11 @@ import java.util.regex.Pattern;
 
 public class Task3 {
 
-	//Дана строка “Versions: Java  5, Java 6, Java   7, Java 8, Java 12.”  
-	//Найти все подстроки "Java <X>" и распечатать их.
-	//
 	public static void main(String[] args) {
 		String str = "Versions: Java  5, Java 6, Java   7, Java 8, Java 12.";
 		Pattern pattern = Pattern.compile("/[Java]+\\s*\\d*");
 		Matcher matcher = pattern.matcher(str);
         System.out.println(pattern.flags());
         }
-	}
 
 }
