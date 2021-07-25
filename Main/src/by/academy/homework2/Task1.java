6da810eb23a73c466cc2af1a3f6bc7648e4deb5f
alexkoval6030 <alexkoval6030@gmail.com>
2021-07-26 00:03:47 +0300

    Homework2 all

--- Main/src/by/academy/homework2/Task1.java
+++ Main/src/by/academy/homework2/Task1.java
@@ -5,35 +5,34 @@ import java.util.Scanner;
 
 public class Task1 {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Введите первое слово: ");
-		String firstWord = scan.nextLine();
-		System.out.println("Введите второе слово: ");
-		String secondWord = scan.nextLine();
-		char[] firstWordArray = firstWord.toCharArray();
-		char[] secondWordArray = secondWord.toCharArray();
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите первое слово: ");
+        String firstWord = scan.nextLine();
+        System.out.println("Введите второе слово: ");
+        String secondWord = scan.nextLine();
+        char[] firstWordArray = firstWord.toCharArray();
+        char[] secondWordArray = secondWord.toCharArray();
 
-		if (firstWord.length() == secondWord.length()) {
-			for (int i = 0; i < secondWordArray.length; i++) {
-				for (int j = 0; j <= firstWordArray.length - 1; j++) {
-					if (secondWord.charAt(i) == firstWord.charAt(j)) {
-						firstWordArray[j] = ' ';
-					}
-				}
-			}
-			firstWord = String.valueOf(firstWordArray);
-			if (firstWord.isBlank() == true) {
-				System.out.println("true");
-			} else {
-				System.out.println("false");
-			}
-		} else {
-			System.out.println("false");
-		}
+        if ((firstWord.length() != secondWord.length())) {
+            System.out.println("False");
+            scan.close();
+            return;
+        }
 
-		scan.close();
-
-	}
-
+        for (int i = 0; i < secondWordArray.length; i++) {
+            for (int j = 0; j < firstWordArray.length; j++) {
+                if (secondWord.charAt(i) == firstWord.charAt(j)) {
+                    firstWordArray[j] = ' ';
+                }
+            }
+        }
+        firstWord = String.valueOf(firstWordArray);
+        if (firstWord.trim().isEmpty()) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+        scan.close();
+    }
 }
--- Main/src/by/academy/homework2/Task2.java
+++ Main/src/by/academy/homework2/Task2.java
@@ -4,27 +4,43 @@ import java.util.Scanner;
 
 public class Task2 {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Введите число сравниваемых слов");
-		int numberOfWordsToCompare = scan.nextInt();
-		String[] wordArray = new String[numberOfWordsToCompare];
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число сравниваемых слов");
+        int numberOfWordsToCompare = scan.nextInt();
+        String[] wordArray = new String[numberOfWordsToCompare];
 
-		for (int i = 0; i < wordArray.length; i++) {
-			System.out.println("Введите слово");
-			wordArray[i] = scan.next();
-		}
-		
-		String wordWithMinimumNumberOfCharacters = wordArray[0];
+        for (int i = 0; i < wordArray.length; i++) {
+            System.out.println("Введите слово");
+            wordArray[i] = scan.next();
+        }
 
-		for (int j = 1; j < wordArray.length; j++) {
-			if (wordArray[j].length() < wordWithMinimumNumberOfCharacters.length()) {
-				wordWithMinimumNumberOfCharacters = wordArray[j];
-			}
-		}
+        int minUnique = Integer.MAX_VALUE;
+        int minIndex = -1;
 
-		System.out.println(wordWithMinimumNumberOfCharacters);
-		scan.close();
-	}
+        for (int j = 0; j < wordArray.length; j++) {
+            String word = wordArray[j];
+            char[] chars = word.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                for (int k = i + 1; k < chars.length; k++) {
+                    if (chars[i] == chars[k]) {
+                        chars[k] = ' ';
+                    }
+                }
+            }
+            int uniqueLetterCount = 0;
+            for (int i = 0; i < chars.length; i++) {
+                if (chars[i] != ' ') {
+                    uniqueLetterCount++;
+                }
+            }
+            if (uniqueLetterCount < minUnique) {
+                minUnique = uniqueLetterCount;
+                minIndex = j;
+            }
+        }
 
+        System.out.println(wordArray[minIndex]);
+        scan.close();
+    }
 }
