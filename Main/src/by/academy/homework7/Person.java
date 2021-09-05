d6264955176e620082e3c07a16c029491aeef044
alexkoval6030 <alexkoval6030@gmail.com>
2021-09-05 22:50:38 +0300

    homework7

---
+++ Main/src/by/academy/homework7/Person.java
@@ -0,0 +1,50 @@
+package by.academy.homework7;
+
+import java.time.LocalDate;
+
+public class Person {
+	
+	public String firstName;
+	public String lastName;
+	private LocalDate dateOfBirth;
+	
+	public Person() {}
+
+	public Person(String firstName, String lastName, LocalDate dateOfBirth) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Person [firstName=");
+		builder.append(firstName);
+		builder.append(", lastName=");
+		builder.append(lastName);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+}
